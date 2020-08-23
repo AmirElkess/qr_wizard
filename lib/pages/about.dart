6e3e951751f53cd5ec1cd1a13b800c78a96c18e1
AmirElkess <amir.elkess@gmail.com>
2020-08-23 23:38:13 +0200

    created license res file about check point

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qr_wizard/res/constants.dart';
+import 'package:qr_wizard/res/button.dart';
+import 'package:qr_wizard/res/licenses.dart';
 
 class About extends StatefulWidget {
   @override
@@ -9,9 +12,63 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('ABOUT_SCREEN'),
-      ),
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text(
+            "ABOUT",
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: SoftButton(
+            margin: 8,
+            radius: 24,
+            width: 5,
+            height: 5,
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: universalPadding,
+            child: SoftButton(
+              radius: 12,
+              width: double.infinity,
+              height: 500,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(14, 14, 14, 14),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Who am I", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("An independent Hobbyist/Developer who's interested in Flutter app development."),
+                    SizedBox(height: 12,),
+                    Text("Contact me", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Email: amir.elkess@gmail.com"),
+                    SizedBox(height: 12,),
+                    Text("Open source licenses", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Flutter", style: TextStyle(fontStyle: FontStyle.italic), ),
+                    SoftButton(
+                      radius: 8,
+                      width: double.infinity,
+                      child: Text(flutterLicense),
+                    ),
+                    SizedBox(height: 12,),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
     );
   }
 }
+
