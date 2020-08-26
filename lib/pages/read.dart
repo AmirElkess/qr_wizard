70403c21c919c7ff27c7dfebc80d5d63aab6be45
AmirElkess <amir.elkess@gmail.com>
2020-08-26 23:39:09 +0200

    detects if scanned code is url if url: highlight for launch else: normal text

--- lib/pages/read.dart
+++ lib/pages/read.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:qr_wizard/res/constants.dart';
 import 'package:qr_wizard/res/button.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Read extends StatefulWidget {
   @override
@@ -11,7 +13,6 @@ class Read extends StatefulWidget {
 }
 
 class _ReadState extends State<Read> {
-
   double notificationOpacityLevel = 0;
   bool FLASH_ON = false;
   Icon flashIcon = Icon(Icons.flash_off);
@@ -20,7 +21,8 @@ class _ReadState extends State<Read> {
   bool playing = true;
   Icon playPause = Icon(Icons.play_arrow);
 
-  var qrText = '';
+  Widget qrText = Text('');
+  String qrTextString = '';
   QRViewController controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
@@ -53,179 +55,210 @@ class _ReadState extends State<Read> {
       body: Align(
         alignment: Alignment.center,
         child: Padding(
-          padding: universalPadding,
-          child: Builder(builder:  (context) {
-            return Column(
-              children: <Widget>[
-                SoftButton(
-                  width: double.infinity,
-                  height: 420,
-                  radius: 12,
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(14, 14, 14, 0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                  color: shadowColor, offset: Offset(4, 4), blurRadius: 2),
-                              BoxShadow(
-                                  color: lightShadowColor,
-                                  offset: Offset(-4, -4),
-                                  blurRadius: 2),
-                            ],
-                          ),
-                          child: SizedBox(
-                            height: 200,
-                            width: double.infinity,
-                            child: QRView(
-                              key: qrKey,
-                              onQRViewCreated: (controller) {
-                                this.controller = controller;
-                                controller.scannedDataStream.listen((event) {
-                                  setState(() {
-                                    qrText = event;
-                                    controller.pauseCamera();
+            padding: universalPadding,
+            child: Builder(builder: (context) {
+              return Column(
+                children: <Widget>[
+                  SoftButton(
+                    width: double.infinity,
+                    height: 420,
+                    radius: 12,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(14, 14, 14, 0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    color: shadowColor,
+                                    offset: Offset(4, 4),
+                                    blurRadius: 2),
+                                BoxShadow(
+                                    color: lightShadowColor,
+                                    offset: Offset(-4, -4),
+                                    blurRadius: 2),
+                              ],
+                            ),
+                            child: SizedBox(
+                              height: 200,
+                              width: double.infinity,
+                              child: QRView(
+                                key: qrKey,
+                                onQRViewCreated: (controller) {
+                                  this.controller = controller;
+                                  controller.scannedDataStream
+                                      .listen((event) async {
+                                        qrTextString = event;
+                                    if (await canLaunch(event)) {
+                                      print("Found URL string");
+                                      qrText = GestureDetector(
+                                        child: Text(
+                                          qrTextString,
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              decoration:
+                                              TextDecoration.underline),
+                                        ),
+                                        onTap: () async {
+                                          await launch(qrTextString);
+                                        },
+                                      );
+                                    } else {
+                                      print("Found normal string");
+                                      qrText = Text(qrTextString);
+                                    }
+                                    setState(() {
+                                      controller.pauseCamera();
                                     playing = false;
                                     playPause = Icon(Icons.pause);
-                                    //print(qrText);
+
+                                    });
                                   });
-                                });
-                              },
-                              overlay: QrScannerOverlayShape(
-                                borderColor: Colors.blueGrey,
-                                borderRadius: 5,
-                                borderLength: 60,
-                                borderWidth: 5,
-                                cutOutSize: 200,
+                                },
+                                overlay: QrScannerOverlayShape(
+                                  borderColor: Colors.blueGrey,
+                                  borderRadius: 5,
+                                  borderLength: 60,
+                                  borderWidth: 5,
+                                  cutOutSize: 200,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          SoftButton(
-                            child: flashIcon,
-                            radius: 50,
-                            onTap: () {
-                              setState(() {
-                                if (FLASH_ON){
-                                  flashIcon = Icon(Icons.flash_off);
-                                } else {
-                                  flashIcon = Icon(Icons.flash_on, color: Colors.yellow,);
-                                }
-                                FLASH_ON = !FLASH_ON;
-                                controller.toggleFlash();
-                              });
-                            },
-                          ),
-                          SoftButton(
-                            child: camIcon,
-                            radius: 50,
-                            onTap: () {
-                              setState(() {
-                                if (REAR_CAM){
-                                  camIcon = Icon(Icons.camera_front);
-                                } else {
-                                  camIcon = Icon(Icons.camera_rear);
-                                }
-                                REAR_CAM = !REAR_CAM;
-                                controller.flipCamera();
-                              });
-                            },
-
-                          ),
-                          SoftButton(
-                            child: playPause,
-                            radius: 50,
-                            onTap: () {
-                              setState(() {
-                                if (playing){
-                                  playPause = Icon(Icons.pause);
-                                  controller.pauseCamera();
-                                } else {
-                                  playPause = Icon(Icons.play_arrow);
-                                  controller.resumeCamera();
-                                }
-                                playing = !playing;
-                              });
-                            },
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Divider(thickness: 1.5, indent: 14, endIndent: 14,),
-                      SizedBox(height: 8,),
-                      SizedBox(
-                        height: 100,
-                        width: double.infinity,
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                flex: 6,
-                                child: SoftButton(
-                                  height: 150,
-                                  radius: 12,
-                                  child: SingleChildScrollView(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(9.0),
-                                      child: Text(qrText),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            SoftButton(
+                              child: flashIcon,
+                              radius: 50,
+                              onTap: () {
+                                setState(() {
+                                  if (FLASH_ON) {
+                                    flashIcon = Icon(Icons.flash_off);
+                                  } else {
+                                    flashIcon = Icon(
+                                      Icons.flash_on,
+                                      color: Colors.yellow,
+                                    );
+                                  }
+                                  FLASH_ON = !FLASH_ON;
+                                  controller.toggleFlash();
+                                });
+                              },
+                            ),
+                            SoftButton(
+                              child: camIcon,
+                              radius: 50,
+                              onTap: () {
+                                setState(() {
+                                  if (REAR_CAM) {
+                                    camIcon = Icon(Icons.camera_front);
+                                  } else {
+                                    camIcon = Icon(Icons.camera_rear);
+                                  }
+                                  REAR_CAM = !REAR_CAM;
+                                  controller.flipCamera();
+                                });
+                              },
+                            ),
+                            SoftButton(
+                              child: playPause,
+                              radius: 50,
+                              onTap: () {
+                                setState(() {
+                                  if (playing) {
+                                    playPause = Icon(Icons.pause);
+                                    controller.pauseCamera();
+                                  } else {
+                                    playPause = Icon(Icons.play_arrow);
+                                    controller.resumeCamera();
+                                  }
+                                  playing = !playing;
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Divider(
+                          thickness: 1.5,
+                          indent: 14,
+                          endIndent: 14,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        SizedBox(
+                          height: 100,
+                          width: double.infinity,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 6,
+                                  child: SoftButton(
+                                    height: 150,
+                                    radius: 12,
+                                    child: SingleChildScrollView(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(9.0),
+                                        child: qrText,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: SoftButton(
-                                  height: 150,
-                                  radius: 12,
-                                  child: Icon(Icons.content_copy),
-                                  onTap: (){
-                                    setState(() {
-                                      Clipboard.setData(ClipboardData(text: qrText));
-                                      notificationOpacityLevel = 1;
-                                      print(notificationOpacityLevel);
-                                      new Timer(Duration(seconds: 2), (){
-                                        setState(() {
-                                          notificationOpacityLevel = 0;
+                                Expanded(
+                                  flex: 1,
+                                  child: SoftButton(
+                                    height: 150,
+                                    radius: 12,
+                                    child: Icon(Icons.content_copy),
+                                    onTap: () {
+                                      setState(() {
+                                        Clipboard.setData(
+                                            ClipboardData(text: qrTextString));
+                                        notificationOpacityLevel = 1;
+                                        print(notificationOpacityLevel);
+                                        new Timer(Duration(seconds: 2), () {
+                                          setState(() {
+                                            notificationOpacityLevel = 0;
+                                          });
                                         });
                                       });
-                                    });
-                                  },
+                                    },
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                AnimatedOpacity(
-                  opacity: notificationOpacityLevel,
-                  duration: Duration(milliseconds: 300),
-                  child: SoftButton(
-                    height: 30,
-                    radius: 12,
-                    width: double.infinity,
-                    child: Text("Text copied to clipboard"),
+                  SizedBox(
+                    height: 10,
                   ),
-                )
-              ],
-            );
-          })
-        ),
+                  AnimatedOpacity(
+                    opacity: notificationOpacityLevel,
+                    duration: Duration(milliseconds: 300),
+                    child: SoftButton(
+                      height: 30,
+                      radius: 12,
+                      width: double.infinity,
+                      child: Text("Text copied to clipboard"),
+                    ),
+                  )
+                ],
+              );
+            })),
       ),
     );
   }
