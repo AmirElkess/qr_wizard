a647b6b86648db6bc4d820370264cfdfa465aec7
AmirElkess <amir.elkess@gmail.com>
2020-08-27 00:39:09 +0200

    updated licenses.dart removed url Regex pattern string from constants.dart

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -41,7 +41,7 @@ class _AboutState extends State<About> {
             child: SoftButton(
               radius: 12,
               width: double.infinity,
-              height: 1850,
+              height: 2100,
               child: Padding(
                 padding: EdgeInsets.fromLTRB(14, 14, 14, 14),
                 child: Column(
@@ -121,6 +121,20 @@ class _AboutState extends State<About> {
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Text(screenshotLicense),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 12,),
+
+                    Text("url_launcher", style: TextStyle(fontStyle: FontStyle.italic), ),
+                    SoftButton(
+                      radius: 8,
+                      width: double.infinity,
+                      height: 250,
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(url_launcherLicense),
                         ),
                       ),
                     ),
