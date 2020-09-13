b01805ae20b9a70652e504df4edbc7851dff2d2a
AmirElkess <amir.elkess@gmail.com>
2020-09-13 23:18:06 +0200

    added bottom universal padding for untrimmed edge shadows

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -8,7 +8,7 @@ Color lightShadowColor = Color(0xFFf3ffff);
 Color textColor = Colors.black54;
 Color seekBarLightColor = Color(0xFFB8ECED);
 Color seekBarDarkColor = Color(0xFF37C8DF);
-EdgeInsets universalPadding = EdgeInsets.fromLTRB(20, 20, 20, 0);
+EdgeInsets universalPadding = EdgeInsets.fromLTRB(20, 20, 20, 5);
 
 
 //String urlPattern = r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)';
