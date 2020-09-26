597824cadc1937e1add563be0fd2280641dc358d
AmirElkess <amir.elkess@gmail.com>
2020-09-26 20:18:15 +0200

    Create page notifies through snackbar instead of custom notification

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -20,7 +20,6 @@ class _CreateState extends State<Create> {
   ScreenshotController screenshotController = ScreenshotController();
   final textController = TextEditingController();
   String qrInput = "";
-  double notificationOpacityLevel = 0;
 
   @override
   void dispose() {
@@ -31,162 +30,146 @@ class _CreateState extends State<Create> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      backgroundColor: backgroundColor,
-      appBar: AppBar(
+        resizeToAvoidBottomPadding: false,
         backgroundColor: backgroundColor,
-        elevation: 0.0,
-        title: Text(
-          "Create QR",
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        leading: SoftButton(
-          margin: 8,
-          radius: 24,
-          width: 5,
-          height: 5,
-          shadowOffset: 0,
-          blurRadius: 0,
-
-          child: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0.0,
+          title: Text(
+            "Create QR",
+            style: TextStyle(color: Colors.black),
           ),
-          onTap: () {
-            Navigator.pop(context);
-          },
+          centerTitle: true,
+          leading: SoftButton(
+            margin: 8,
+            radius: 24,
+            width: 5,
+            height: 5,
+            shadowOffset: 0,
+            blurRadius: 0,
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: universalPadding,
-          child: Column(
-            children: <Widget>[ 
-              SoftButton(
-                radius: 12,
-                width: double.infinity,
-                height: 400,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 5,
-                      child: Row(children: <Widget>[
-                        Expanded(
-                          flex: 6,
-                          child: SoftButton(
-                            radius: 12,
-                            height: double.infinity,
-                            width: double.infinity,
-                            child: Screenshot(
-                              controller: screenshotController,
-                              child: QrImage(
-                                data: qrInput,
-                                version: QrVersions.auto,
-                                size: 200.0,
-                                backgroundColor: backgroundColor,
+        body: Builder(
+          builder: (BuildContext context) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: universalPadding,
+                child: SoftButton(
+                  radius: 12,
+                  width: double.infinity,
+                  height: 400,
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 5,
+                        child: Row(children: <Widget>[
+                          Expanded(
+                            flex: 6,
+                            child: SoftButton(
+                              radius: 12,
+                              height: double.infinity,
+                              width: double.infinity,
+                              child: Screenshot(
+                                controller: screenshotController,
+                                child: QrImage(
+                                  data: qrInput,
+                                  version: QrVersions.auto,
+                                  size: 200.0,
+                                  backgroundColor: backgroundColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: SoftButton(
+                              width: double.infinity,
+                              height: double.infinity,
+                              child: Icon(Icons.clear),
+                              isClickable: true,
+                              onTap: () {
+                                setState(() {
+                                  textController.value =
+                                      TextEditingValue(text: "");
+                                  qrInput = "";
+                                });
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: SoftButton(
+                              height: double.infinity,
+                              width: double.infinity,
+                              child: Icon(Icons.save_alt),
+                              isClickable: true,
+                              onTap: () {
+                                setState(() {
+                                  FocusScope.of(context).unfocus();
+                                  screenshotController
+                                      .capture(
+                                          pixelRatio: 2,
+                                          delay: Duration(milliseconds: 150))
+                                      .then((File image) async {
+                                    if (image != null && image.path != null) {
+                                      print("Image and path correct");
+                                      print(image.path);
+                                      await GallerySaver.saveImage(
+                                        image.path,
+                                        albumName: "QR Wizard",
+                                      );
+                                      Scaffold.of(context).showSnackBar(
+                                          SnackBar(
+                                              content:
+                                                  Text('QR saved to gallery')));
+                                    }
+                                  });
+                                });
+                              },
+                            ),
+                          )
+                        ]),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: SoftButton(
+                          radius: 12,
+                          width: double.infinity,
+                          height: double.infinity,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(15, 0, 15, 5),
+                            child: TextField(
+                              controller: textController,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 5,
+                              maxLength: 120,
+                              onChanged: (text) {
+                                setState(() {
+                                  qrInput = text;
+                                  print(qrInput);
+                                });
+                              },
+                              decoration: InputDecoration(
+                                hintText: 'Enter text here',
+                                border: InputBorder.none,
                               ),
                             ),
                           ),
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: SoftButton(
-                            width: double.infinity,
-                            height: double.infinity,
-                            child: Icon(Icons.clear),
-                            isClickable: true,
-                            onTap: () {
-                              setState(() {
-                                textController.value =
-                                    TextEditingValue(text: "");
-                                qrInput = "";
-                              });
-                            },
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: SoftButton(
-                            height: double.infinity,
-                            width: double.infinity,
-                            child: Icon(Icons.save_alt),
-                            isClickable: true,
-                            onTap: () {
-                              setState(() {
-                                FocusScope.of(context).unfocus();
-                                screenshotController
-                                    .capture(pixelRatio: 2, delay: Duration(milliseconds: 150))
-                                    .then((File image) async {
-                                  if (image != null && image.path != null) {
-                                    print("Image and path correct");
-                                    print(image.path);
-                                    await GallerySaver.saveImage(image.path, albumName: "QR Wizard",);
-                                    setState(() {
-                                      notificationOpacityLevel = 1;
-                                    });
-                                    new Timer(Duration(seconds: 2), () {
-                                      setState(() {
-                                        notificationOpacityLevel = 0;
-                                      });
-                                    });
-
-                                  }
-
-                                });
-                              });
-                            },
-                          ),
-                        )
-                      ]),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: SoftButton(
-                        radius: 12,
-                        width: double.infinity,
-                        height: double.infinity,
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(15, 0, 15, 5),
-                          child: TextField(
-                            controller: textController,
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 5,
-                            maxLength: 120,
-                            onChanged: (text) {
-                              setState(() {
-                                qrInput = text;
-                                print(qrInput);
-                              });
-                            },
-                            decoration: InputDecoration(
-                              hintText: 'Enter text here',
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 10,
-              ),
-              AnimatedOpacity(
-                opacity: notificationOpacityLevel,
-                duration: Duration(milliseconds: 300),
-                child: SoftButton(
-                  height: 31,
-                  radius: 12,
-                  width: double.infinity,
-                  child: Text("QR saved successfully"),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
